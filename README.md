173d96b647ab3d4ac3483db54a566ff0e770613f
Ash Kyd <ash@kyd.com.au>
2016-05-21 00:56:06 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# zeekay/nlink
+Just a bunch of dotfiles.
+
+## Install
+Clone and symlink or install with [ellipsis][ellipsis]:
+
+```
+$ ellipsis install zeekay/nlink
+```
+
+[ellipsis]: http://ellipsis.sh
---
+++ ellipsis.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# zeekay/nlink ellipsis package
+
+# The following hooks can be defined to customize behavior of your package:
+# pkg.install() {
+#     fs.link_files $PKG_PATH
+# }
+
+# pkg.push() {
+#     git.push
+# }
+
+# pkg.pull() {
+#     git.pull
+# }
+
+# pkg.installed() {
+#     git.status
+# }
+#
+# pkg.status() {
+#     git.diffstat
+# }
